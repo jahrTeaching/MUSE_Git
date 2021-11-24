71ae31183057354d5955672155d26298348a1f7e
Juan Antonio Hernandez Ramos <jahramos@gmail.com>
2021-11-24 23:27:11 +0100

    Add project files.

---
+++ MUSE_Git.f90
@@ -0,0 +1,25 @@
+!  MUSE_Git.f90 
+!
+!  FUNCTIONS:
+!  MUSE_Git - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: MUSE_Git
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program MUSE_Git
+
+    implicit none
+
+    ! Variables
+
+    ! Body of MUSE_Git
+    print *, 'Hello World'
+
+    end program MUSE_Git
+
