b1c0bae4c330e129f0a7336c9180e3c0f842152d
Juan Antonio Hernandez Ramos <jahramos@gmail.com>
2021-12-01 21:41:02 +0100

    Update MUSE_Git.f90

--- MUSE_Git.f90
+++ MUSE_Git.f90
@@ -1,25 +1,10 @@
-!  MUSE_Git.f90 
-!
-!  FUNCTIONS:
-!  MUSE_Git - Entry point of console application.
-!
 
-!****************************************************************************
-!
-!  PROGRAM: MUSE_Git
-!
-!  PURPOSE:  Entry point for the console application.
-!
-!****************************************************************************
 
     program MUSE_Git
 
-    implicit none
-
-    ! Variables
-
-    ! Body of MUSE_Git
-    print *, 'Hello World'
+       implicit none
+       
+       write(*,*) 'Hello World'
 
     end program MUSE_Git
 
